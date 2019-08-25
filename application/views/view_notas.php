3822a22d70dc2e9d0c9baa49b2325e556fe190d0
veriito-dus <53842300+veriito-dus@users.noreply.github.com>
2019-08-25 00:49:59 -0500

    Add files via upload

---
+++ application/views/view_notas.php
@@ -0,0 +1,360 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>NOTAS</title>
+        <ul class="nav justify-content-end  nav-pills">
+            <li class="">
+                <a class="nav-link active" href="#">CERRAR SECCION</a>
+            </li>
+            </ul>
+                <h1 style="font-size: 28px; text-align: center" >INSTITUCION EDUCATIVA</h1>
+                
+                <ul class="nav justify-content-center nav nav-tabs">
+            <li class="">
+                <a class="nav-link active" href="#">PLANILLAS</a>
+            </li>
+            <li class="">
+                <a class="nav-link" href="#">HORARIO</a>
+            </li>
+            <li class="">
+                <a class="nav-link" href="#">INICIO</a>
+            </li>
+        </ul>
+
+    </head>
+    <body background="img\TRES.jpg" style="background-size: 100%">
+    <div style="position: relative; left: 1200px">
+            <img src="img/col.png" alt="" width="100" height="90">        
+        </div>
+        <div style="position: relative; left: 1160px">
+            <h1 style="font-size: 18px" >VERONICA DUSSAN</h1>  
+        </div>
+        <h1 style="text-align: center; font-size: 45px ;text-shadow: 4px 2px #ffffff;">NOTAS</h1>
+        <div class="container">
+    <div class="row">
+        <div class="col-md-3 col-sm-3 col-xs-3">
+        </div>
+        <div class="col-md-2 col-sm-2 col-xs-2">
+            <h1 style="font-size: 18px; margin-top: 30px;color:#ffffff" >GRADO</h1>
+            <input type="text" name="GRADO" placeholder="" style="width:150px; height:30px">
+            
+        </div>
+        <div class="col-md-2 col-sm-2 col-xs-2">
+            <h1 style="font-size: 18px; margin-top: 30px ;color:#ffffff" >PERIODO</h1>
+            <input type="text" name="PERIODO" placeholder=" " style="width:150px; height:30px">
+        </div>
+
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <h1 style="font-size: 18px; margin-top: 30px;color:#ffffff" >MATERIA</h1>
+            <input type="text" name="MATERIA" placeholder=" " style="width:150px; height:30px">
+        </div>
+        <table class="table table-bordered" style= " text-align: center;background-color:#ffffff;margin-top: 30px">
+    <thead>
+    <tr>
+      <th scope="col">NUMERO</th>
+      <th scope="col">NOMBRE ESTUDIANTE</th>
+      <th scope="col">-</th>
+      <th scope="col">-</th>
+      <th scope="col">-</th>
+      <th scope="col">DEF</th>
+      <th scope="col">-</th>
+      <th scope="col">-</th>
+      <th scope="col">-</th>
+      <th scope="col">DEF</th>
+      <th scope="col">PRUEBAS SABER</th>
+      <th scope="col">DEFINITIVA</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">NUM</th>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+
+        
+</body>
+</html>
